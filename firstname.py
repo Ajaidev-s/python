0ca78b82542e76bb21c17b8b80056107c139eaab
AJAIDEV S <146339839+ajwildr@users.noreply.github.com>
2023-10-03 15:42:59 +0530

    Update firstname.py

--- firstname.py
+++ firstname.py
@@ -1,8 +1,12 @@
-names=["ajai","Amal","sanal","rajesh","ravi","ajmal"]
-names=[item.lower() for item in names]
+# names=["ajai dev","Amal das","sanal ki","rajesh ravi","ravi ","ajmal ramu"]
+s1=input("Enter names seperated by commas")
+names=list(s1.split(','))
+names=[item.split(' ') for item in names]
 print(names)
 count=0
+print("first names")
 for item in names:
-    if(item[0]=='a'):
+    print(item[0])
+    if(item[0][0]=='a' or item[0][0]=='A' ):
         count=count+1
 print("No of names start with A is ",count)
