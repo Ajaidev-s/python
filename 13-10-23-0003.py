396b0d49c0fef88739b9fba67e0ef1dd53560639
AJAIDEV S <146339839+ajwildr@users.noreply.github.com>
2023-10-13 15:52:35 +0530

    Add files via upload

---
+++ 13-10-23-0003.py
@@ -0,0 +1,15 @@
+#7
+clr1=set(input("enter colours seperated by space ").split())
+clr2=set(input("enter colours seperated by space ").split())
+print(clr1-clr2)
+#8
+d1={"name":"ajai","batch":"mca","mark1":23}
+d2={"mark2":56,"mark3":89}
+d1.update(d2)
+print(d1)
+
+#9
+print(sorted(d1.items(),reverse=True))
+
+#10
+
