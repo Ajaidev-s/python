7a7540ef28e16facca6031ee5849d216af22de44
AJAIDEV S <146339839+ajwildr@users.noreply.github.com>
2023-10-13 14:13:48 +0530

    Add files via upload

---
+++ 13-10-23.py
@@ -0,0 +1,3 @@
+#1,write a program to determine frequency of a word in a sentence
+#2,write a program to determine frequency of alphabets in a word
+#3,write a program to create an inverted dictionary
