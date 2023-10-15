56e9b320484c89a8d3cb0631103e395fca3b10ab
AJAIDEV S <146339839+ajwildr@users.noreply.github.com>
2023-10-15 11:18:06 +0530

    Update 13-10-23-0003.py

--- 13-10-23-0003.py
+++ 13-10-23-0003.py
@@ -1,8 +1,8 @@
-#7
+#7 To print all colours from colours-list1 not contained in colours-list2
 clr1=set(input("enter colours seperated by space ").split())
 clr2=set(input("enter colours seperated by space ").split())
 print(clr1-clr2)
-#8
+#8 to merge 2 dictionaries
 d1={"name":"ajai","batch":"mca","mark1":23}
 d2={"mark2":56,"mark3":89}
 d1.update(d2)
