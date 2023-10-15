1cccd629bf73f4c48094ca3c2060f5397515a735
AJAIDEV S <146339839+ajwildr@users.noreply.github.com>
2023-10-15 08:31:57 +0530

    Update 13-10-23-0002.py

--- 13-10-23-0002.py
+++ 13-10-23-0002.py
@@ -8,11 +8,12 @@ print("Is there sums are same: ",sum(num1)==sum(num2))
 print("they have common elements:",bool(len(set(num1)&set(num2))))
 
 #5
+# To write a python program to add 'ing' at the end of the string.if already ends with 'ing' then add 'ly'
 s1=input("enter a string")
 s1 += 'ly' if s1.endswith('ing') else 'ing'
 print(s1)
 
-#6
+#6 to accept a string and return the length of longest word if the result has more than one word ,then print bingo
 words=input("enter the string").split()
 words.sort(key=len,reverse=True)
 print("max length=",len(words[0]))
